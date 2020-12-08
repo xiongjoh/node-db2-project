28f208230b99f0107f3ac0bea7bd07c1cca10c92
unknown <xiongjoh@gmail.com>
2020-12-08 13:08:59 -0600

    install dependencies, add api/data/index

---
+++ data/dbConfig.js
@@ -0,0 +1,9 @@
+const knex = require('knex');
+
+const config = require('../knexfile');
+
+const db = knex(process.env.NODE_ENV === 'production' 
+    ? config.production
+    : config.development);
+
+module.exports = db;
